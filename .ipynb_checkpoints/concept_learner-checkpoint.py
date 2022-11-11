c3c3efd95fe0267ba266e1d197adbd058b9def2e
Arlind Iseni <aris20@bth.student.se>
2022-11-11 15:24:59 +0100

    refactoring

--- .ipynb_checkpoints/concept_learner-checkpoint.py
+++ .ipynb_checkpoints/concept_learner-checkpoint.py
@@ -86,10 +86,6 @@ class ConceptLearner:
         return self._lgg_series
     
     @property
-    def get_conjective_rule(self) -> pd.Series:
-        return self._lgg_series.T
-    
-    @property
-    def get_conjective_rule2(self) -> None:
-        for i, value in self._lgg_series:
-            print(f"{i} = {value} ∧")
+    def get_conjective_rule(self) -> None:
+        for i, value in self._lgg_series.items():
+            print(f"{i} = {value} ∧ ", end="")
